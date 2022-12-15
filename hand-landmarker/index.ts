eb857eccc1a85522ee57647bce531ac7877e44e4
Khanh LeViet <khanhlvg@google.com>
2022-12-15 13:19:10 -0800

    Added hand landmark samples.

---
+++ hand-landmarker/index.ts
@@ -0,0 +1,184 @@
+// Copyright 2022 The MediaPipe Authors.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//      http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { HandLandmarker, FilesetResolver } from '@mediapipe/tasks-vision';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
+import { HAND_CONNECTIONS } from '@mediapipe/hands';
+
+const demosSection = document.getElementById('demos');
+
+let handLandmarker;
+let runningMode: 'image' | 'video' = 'image';
+let enableWebcamButton: HTMLButtonElement;
+let webcamRunning: Boolean = false;
+const videoHeight = '360px';
+const videoWidth = '480px';
+
+// Before we can use HandLandmarker class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment to
+// get everything needed to run.
+async function runDemo() {
+  // Read more `CopyWebpackPlugin`, copy wasm set from `node_modules` to `/wasm`
+  const vision = await FilesetResolver.forVisionTasks('/wasm');
+  handLandmarker = await HandLandmarker.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-assets/hand_landmarker.task`,
+    },
+    runningMode,
+    numHands: 2
+  });
+  demosSection.classList.remove('invisible');
+}
+runDemo();
+
+/********************************************************************
+// Demo 1: Grab a bunch of images from the page and detection them
+// upon click.
+********************************************************************/
+
+// In this demo, we have put all our clickable images in divs with the
+// CSS class 'detectionOnClick'. Lets get all the elements that have
+// this class.
+const imageContainers = document.getElementsByClassName('detectOnClick');
+
+// Now let's go through all of these and add a click event listener.
+for (let i = 0; i < imageContainers.length; i++) {
+  // Add event listener to the child element whichis the img element.
+  imageContainers[i].children[0].addEventListener('click', handleClick);
+}
+
+// When an image is clicked, let's detect it and display results!
+async function handleClick(event) {
+  if (!handLandmarker) {
+    console.log('Wait for handLandmarker to load before clicking!');
+    return;
+  }
+
+  if (runningMode === 'video') {
+    runningMode = 'image';
+    await handLandmarker.setOptions({ runningMode });
+  }
+  // Remove all landmarks drawed before
+  const allCanvas = event.target.parentNode.getElementsByClassName('canvas');
+  for (var i = allCanvas.length - 1; i >= 0; i--) {
+    const n = allCanvas[i];
+    n.parentNode.removeChild(n);
+  }
+
+  // We can call handLandmarker.detect as many times as we like with
+  // different image data each time. This returns a promise
+  // which we wait to complete and then call a function to
+  // print out the results of the prediction.
+  const handLandmarkerResult = handLandmarker.detect(event.target);
+  const canvas = document.createElement('canvas') as HTMLCanvasElement;
+  canvas.setAttribute('class', 'canvas');
+  canvas.setAttribute('width', event.target.naturalWidth + 'px');
+  canvas.setAttribute('height', event.target.naturalHeight + 'px');
+  canvas.style.left = '0px';
+  canvas.style.top = '0px';
+  canvas.style.width = `${event.target.width}px`;
+  canvas.style.height = `${event.target.height}px`;
+
+  event.target.parentNode.appendChild(canvas);
+  const cxt = canvas.getContext('2d');
+  for (const landmarks of handLandmarkerResult.landmarks) {
+    drawConnectors(cxt, landmarks, HAND_CONNECTIONS, {
+      color: '#00FF00',
+      lineWidth: 5
+    });
+    drawLandmarks(cxt, landmarks, { color: '#FF0000', lineWidth: 1 });
+  }
+}
+
+/********************************************************************
+// Demo 2: Continuously grab image from webcam stream and detect it.
+********************************************************************/
+
+const video = document.getElementById('webcam') as HTMLVideoElement;
+const canvasElement = document.getElementById('output_canvas') as HTMLCanvasElement;
+const canvasCtx = canvasElement.getContext('2d');
+
+// Check if webcam access is supported.
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it.
+if (hasGetUserMedia()) {
+  enableWebcamButton = document.getElementById('webcamButton') as HTMLButtonElement;
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
+
+// Enable the live webcam view and start detection.
+function enableCam(event) {
+  if (!handLandmarker) {
+    console.log('Wait! objectDetector not loaded yet.');
+    return;
+  }
+
+  if (webcamRunning === true) {
+    webcamRunning = false;
+    enableWebcamButton.innerText = 'ENABLE PREDICTIONS';
+  } else {
+    console.log('webcam was off');
+    webcamRunning = true;
+    enableWebcamButton.innerText = 'DISABLE PREDICITONS';
+  }
+
+  // getUsermedia parameters.
+  const constraints = {
+    video: true
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
+  });
+}
+
+async function predictWebcam() {
+  canvasElement.style.height = videoHeight;
+  video.style.height = videoHeight;
+  canvasElement.style.width = videoWidth;
+  video.style.width = videoWidth;
+  // Now let's start detecting the stream.
+  if (runningMode === 'image') {
+    runningMode = 'video';
+    await handLandmarker.setOptions({ runningMode: runningMode });
+  }
+  let nowInMs = Date.now();
+  const results = handLandmarker.detectForVideo(video, nowInMs);
+
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  if (results.landmarks) {
+    for (const landmarks of results.landmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+        color: '#00FF00',
+        lineWidth: 5
+      });
+      drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+    }
+  }
+  canvasCtx.restore();
+
+  // Call this function again to keep predicting when the browser is ready.
+  if (webcamRunning === true) {
+    window.requestAnimationFrame(predictWebcam);
+  }
+}
